14ea94539a64acc1ed7ef48754e5f07efd4d26c7
vikram.lodhi <vikram.lodhi@minix.co.in>
2025-03-26 15:39:09 +0530

    glfw from git

---
+++ cmake/Glfw3.cmake
@@ -0,0 +1,16 @@
+include(FetchContent)
+
+FetchContent_Declare(glfw URL https://github.com/glfw/glfw/archive/refs/tags/3.4.zip)
+FetchContent_MakeAvailable(glfw)
+FetchContent_GetProperties(glfw)
+
+message(STATUS "glfw_DIR : ${glfw_SOURCE_DIR}")
+
+set(GLFW_LIBRARY_TYPE STATIC)
+set(GLFW_BUILD_TESTS OFF)
+set(GLFW_BUILD_EXAMPLES OFF)
+set(GLFW_BUILD_WAYLAND OFF)
+set(GLFW_BUILD_DOCS OFF)
+set(GLFW_BUILD_X11 ON)
+
+include_directories(${glfw_SOURCE_DIR})
