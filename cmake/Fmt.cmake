ac8896fc443a0988061b095b3149685e5ece275b
Vikram <vikram.smvdu@gmail.com>
2025-03-28 21:21:38 +0530

    21.21

--- cmake/Fmt.cmake
+++ cmake/Fmt.cmake
@@ -1,10 +1,10 @@
 include(FetchContent)
 
+set(FMT_UNICODE OFF)
 FetchContent_Declare(fmt URL https://github.com/fmtlib/fmt/releases/download/11.1.4/fmt-11.1.4.zip)
-
 FetchContent_MakeAvailable(fmt)
 FetchContent_GetProperties(fmt)
 
-set(FMT_UNICODE OFF)
+
 message(STATUS "fmt_DIR : ${fmt_SOURCE_DIR}")
 include_directories(${fmt_SOURCE_DIR}/include)
