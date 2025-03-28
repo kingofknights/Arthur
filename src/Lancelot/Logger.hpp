593244a34c0bb1375d7b29dde8117b0840658bc3
vikram.lodhi <vikram.lodhi@minix.co.in>
2025-03-28 15:08:54 +0530

    15_08

--- src/Lancelot/Logger.hpp
+++ src/Lancelot/Logger.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <fmt/chrono.h>
 #include <fmt/color.h>
 #include <fmt/core.h>
 #include <fmt/printf.h>
@@ -12,7 +13,7 @@
 #define LOG(TYPE, format, ...)   LOG_##TYPE(format, __VA_ARGS__)
 
 // Info Log with Green Color
-#define LOG_INFO(format, ...)    fmt::print(stderr, fmt::fg(fmt::color::green), "{:%Y-%m-%d %H:%M:%S} " format "\n", fmt : localtime(std::time(0)), __VA_ARGS__);
+#define LOG_INFO(format, ...)    fmt::print(stderr, fmt::fg(fmt::color::green), "{:%Y-%m-%d %H:%M:%S} " format "\n", fmt ::localtime(std::time(0)), __VA_ARGS__);
 
 // Info Log with Red Color
 #define LOG_ERROR(format, ...)   fmt::print(stderr, fmt::fg(fmt::color::red), "{:%Y-%m-%d %H:%M:%S} " format "\n", fmt::localtime(std::time(0)), __VA_ARGS__);
