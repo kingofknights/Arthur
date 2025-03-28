c3e2f852da49970de71c3c6722fa39d393737627
vikram.lodhi <vikram.lodhi@minix.co.in>
2025-03-28 09:46:49 +0530

    config REQUIRED

--- src/Lancelot/Logger.hpp
+++ src/Lancelot/Logger.hpp
@@ -1,22 +1,22 @@
 #pragma once
 
-#include <fmt/chrono.h>
 #include <fmt/color.h>
 #include <fmt/core.h>
 #include <fmt/printf.h>
 #include <fmt/ranges.h>
 
+#define DISABLE_LOGGING
 #ifdef DISABLE_LOGGING
 #define LOG(...) ;
 #else
 
-#define LOG(TYPE, format, ...) LOG_##TYPE(format, __VA_ARGS__)
+#define LOG(TYPE, format, ...)   LOG_##TYPE(format, __VA_ARGS__)
 
 // Info Log with Green Color
-#define LOG_INFO(format, ...) fmt::print(stderr, fmt::fg(fmt::color::green), "{:%Y-%m-%d %H:%M:%S} " format "\n", fmt::localtime(std::time(0)), __VA_ARGS__);
+#define LOG_INFO(format, ...)    fmt::print(stderr, fmt::fg(fmt::color::green), "{:%Y-%m-%d %H:%M:%S} " format "\n", fmt::localtime(std::time(0)), __VA_ARGS__);
 
 // Info Log with Red Color
-#define LOG_ERROR(format, ...) fmt::print(stderr, fmt::fg(fmt::color::red), "{:%Y-%m-%d %H:%M:%S} " format "\n", fmt::localtime(std::time(0)), __VA_ARGS__);
+#define LOG_ERROR(format, ...)   fmt::print(stderr, fmt::fg(fmt::color::red), "{:%Y-%m-%d %H:%M:%S} " format "\n", fmt::localtime(std::time(0)), __VA_ARGS__);
 
 // Info Log with Purple Color
 #define LOG_WARNING(format, ...) fmt::print(stderr, fmt::fg(fmt::color::purple), "{:%Y-%m-%d %H:%M:%S} " format "\n", fmt::localtime(std::time(0)), __VA_ARGS__);
