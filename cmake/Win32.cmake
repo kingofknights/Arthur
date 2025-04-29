b841a1d57756e1fee03a23afc9ada23ce6f492df
vikram.lodhi <vikram.lodhi@minix.co.in>
2025-04-29 13:23:57 +0530

    big obj

--- cmake/Win32.cmake
+++ cmake/Win32.cmake
@@ -3,6 +3,6 @@ if(WIN32)
   add_compile_definitions(WINVER=0x0A00)
   add_compile_definitions(_WIN32_WINNT=0x0A00)
   add_compile_definitions(_CRT_SECURE_NO_WARNINGS)
-  add_compile_options(/bigobj)
+  # add_compile_options(/bigobj)
 
 endif()
