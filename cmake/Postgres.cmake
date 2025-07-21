476738e0a1e53cd5b6b986b05febe1f11eecd95c
vikram.lodhi <vikram.lodhi@minix.co.in>
2025-07-21 09:57:49 +0530

    modified

--- cmake/Postgres.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(pqxx URL https://github.com/jtv/libpqxx/archive/refs/tags/7.10.1.tar.gz)
-FetchContent_MakeAvailable(pqxx)
-FetchContent_GetProperties(pqxx)
-
-message(STATUS "pqxx_DIR : ${pqxx_SOURCE_DIR}")
-include_directories(${pqxx_SOURCE_DIR}/include)
